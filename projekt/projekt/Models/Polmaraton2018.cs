9f868fe1f98fc3498fe2491169d5622fb8541de0
Kinga Pszeniczna <w57122@student.wsiz.rzeszow.pl>
2019-01-28 20:52:19 +0100

    tworzenie modelu bazy część I

---
+++ projekt/projekt/Models/Polmaraton2018.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projekt.Models
+{
+    public class Polmaraton2018
+    {
+        public int ID { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Miasto { get; set; }
+        public string Klub { get; set; }
+        public string Kraj { get; set; }
+        public DateTime Data_urodzenia { get; set; }
+    }
+}
