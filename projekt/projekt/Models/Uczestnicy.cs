358bf1bab301f54dcdbb3556dda52af503a4b823
Agnieszka <w57097@student.wsiz.rzeszow.pl>
2019-01-29 17:37:24 +0100

    Dodatnie modelu tabeli Uczestnicy

---
+++ projekt/projekt/Models/Uczestnicy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projekt.Models
+{
+    public class Uczestnicy
+    {
+        public int ID_zaw { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Miasto { get; set; }
+        public string Klub { get; set; }
+        public string Kraj { get; set; }
+        public DateTime Data_urodzenia { get; set; }
+        public string Plec { get; set; }
+        public int Wynik_2018 { get; set; }
+        public int Wynik_2017 { get; set; }
+        public int Wynik_2016 { get; set; }
+        public int Wynik_2015 { get; set; }
+        public int Wynik_2014 { get; set; }
+
+    }
+}
