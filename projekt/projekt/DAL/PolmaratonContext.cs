16d1d28a65174f98dd07d6328eab1f9f84be2832
Kinga Pszeniczna <w57122@student.wsiz.rzeszow.pl>
2019-01-29 18:50:19 +0100

    utworzenie kontekstu bazy

---
+++ projekt/projekt/DAL/PolmaratonContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using projekt.Models;
+
+namespace projekt.DAL
+{
+    public class PolmaratonContext :DbContext
+    { 
+        public PolmaratonContext():base("PolmaratonContext")
+        {
+
+        }
+
+        public DbSet<Polmaraton2014> Polmaraton2014s { get; set; }
+        public DbSet<Polmaraton2015> Polmaraton2015s { get; set; }
+        public DbSet<Polmaraton2016> Polmaraton2016s { get; set; }
+        public DbSet<Polmaraton2017> Polmaraton2017s { get; set; }
+        public DbSet<Polmaraton2018> Polmaraton2018s { get; set; }
+        public DbSet<Uczestnicy> Uczestnicies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
---
+++ projekt/projekt/DAL/PolmaratonInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using projekt.Models;
+
+namespace projekt.DAL
+{
+    public class PolmaratonInitializer:
+        System.Data.Entity.DropCreateDatabaseIfModelChanges<PolmaratonContext>
+    {
+        protected override void Seed(PolmaratonContext context)
+        {
+            var uczestnicies = new List<Uczestnicy>
+            {
+                new Uczestnicy{Imie="", Nazwisko="", Miasto="", Klub="", Kraj="", Data_urodzenia=DateTime.Parse("rrrr-mm-dd"), Plec="", Wynik_2014= , Wynik_2015= ,Wynik_2016= ,Wynik_2017= ,Wynik_2018= },
+                new Uczestnicy{Imie="", Nazwisko="", Miasto="", Klub="", Kraj="", Data_urodzenia=DateTime.Parse("rrrr-mm-dd"), Plec="", Wynik_2014= , Wynik_2015= ,Wynik_2016= ,Wynik_2017= ,Wynik_2018= },
+                new Uczestnicy{Imie="", Nazwisko="", Miasto="", Klub="", Kraj="", Data_urodzenia=DateTime.Parse("rrrr-mm-dd"), Plec="", Wynik_2014= , Wynik_2015= ,Wynik_2016= ,Wynik_2017= ,Wynik_2018= }
+
+            };
+
+            uczestnicies.ForEach(s => context.Uczestnicies.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
